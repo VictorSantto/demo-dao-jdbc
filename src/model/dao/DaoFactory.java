a9c2fc5345b9f1daa20f8818ba1b79eed404cccc
VictorSantto <victor.sousa@codiub.com.br>
2023-02-28 12:08:25 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,16 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao creaSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao creaDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 }
